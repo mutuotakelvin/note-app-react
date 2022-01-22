a5efc885276137d8e86b31a7db71349165985ac8
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-22 13:57:39 +0300

    Update NotesList.js

--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Note from './Note'
 import AddNote from './AddNote'
-{/* this component is storing the notes in a format*/}
 
+//this component is storing the notes in a format
 const NotesList=({notes,handleAddNote,handleDelete}) =>{
     return (
         <div className="notes-list">
         {/*here we are going to use .map to make each note card from notes
-        and also passing the functions and other states as propas*/}
+        and also passing the functions and other states as props*/}
            {notes.map((note)=>
                <Note id={note.id} text={note.text} date={note.date}
                handleDelete={handleDelete}
